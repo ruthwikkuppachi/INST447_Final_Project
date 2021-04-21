19f4de73acccb6fa34dda161d47cff4113de58f3
Maya Brenner-Letich <mayabren@terpmail.umd.edu>
2021-04-21 14:09:12 -0400

    Update api.py

--- api.py
+++ api.py
@@ -6,7 +6,7 @@ import pandas as pd
 import numpy as np 
 import requests
 import matplotlib.pyplot as plt
-%matplotlib inline
+
 
 URL = 'https://opendata.arcgis.com/datasets/9fa34e198ad240358c7c36bc063d2058_40.geojson'
 
