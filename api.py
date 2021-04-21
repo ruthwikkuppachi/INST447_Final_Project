b0dc056d0cd991f78e0cc15028f004f5b01fae69
Alicia Afriyie <71097048+aliciaafriyie@users.noreply.github.com>
2021-04-21 14:01:18 -0400

    Update api.py

--- api.py
+++ api.py
@@ -1,6 +1,10 @@
 #https://opendata.arcgis.com/datasets/9fa34e198ad240358c7c36bc063d2058_40.geojson
 
+'''
+Importing Libraries
+'''
 import pandas as pd
 import numpy as np 
-import re
-import request
+import requests
+import matplotlib.pyplot as plt
+%matplotlib inline
