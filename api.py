b42c3e377defd9bdfc067289a16caa2c4a5eb5c3
Alicia Afriyie <71097048+aliciaafriyie@users.noreply.github.com>
2021-04-21 14:05:31 -0400

    Update api.py

--- api.py
+++ api.py
@@ -1,4 +1,3 @@
-#https://opendata.arcgis.com/datasets/9fa34e198ad240358c7c36bc063d2058_40.geojson
 
 '''
 Importing Libraries
@@ -8,3 +7,10 @@ import numpy as np
 import requests
 import matplotlib.pyplot as plt
 %matplotlib inline
+
+URL = 'https://opendata.arcgis.com/datasets/9fa34e198ad240358c7c36bc063d2058_40.geojson'
+
+data = requests.get(URL)
+
+# print html response object
+print(data.request)
