80cb1410b5d4e38042ef998ce638a8548d9d2219
Maya Brenner-Letich <mayabren@terpmail.umd.edu>
2021-04-21 14:11:29 -0400

    Update api.py

--- api.py
+++ api.py
@@ -7,9 +7,8 @@ import numpy as np
 import requests
 import matplotlib.pyplot as plt
 
-
 URL = 'https://opendata.arcgis.com/datasets/9fa34e198ad240358c7c36bc063d2058_40.geojson'
 
 data = requests.get(URL)
 
-data.status_code
+print(data.status_code)
