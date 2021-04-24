13934d804cdb568d002bea3a2d2ce77e873316f9
Maya Brenner-Letich <mayabren@terpmail.umd.edu>
2021-04-23 20:52:10 -0400

    test

--- api.py
+++ api.py
@@ -7,16 +7,17 @@ import numpy as np
 import requests
 import matplotlib.pyplot as plt
 
-URL = 'https://opendata.arcgis.com/datasets/9fa34e198ad240358c7c36bc063d2058_40.geojson'
 
-data = requests.get(URL)
+data = pd.read_csv('Felony_Sentences.csv')
 
-print(data.status_code)
+data.head()
 
-#RID/Race/offesce type/offense/severity - is the severity of similar crimes the same by race?
 
-#cases by race - how many cases are there per race
 
-#type by age and gender
+'''RID/Race/offesce type/offense/severity - is the severity of similar crimes the same by race?'''
 
-#Race/offense type/sentence type/sentence imposted month - are sentences given similar for similar crimes by race?
+'''cases by race - how many cases are there per race?'''
+
+'''type by age and gender'''
+
+'''Race/offense type/sentence type/sentence imposted month - are sentences given similar for similar crimes by race?'''
