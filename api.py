575324d169d5bcc554cb7540c53c015e0bc093ed
Maya Brenner-Letich <mayabren@terpmail.umd.edu>
2021-04-23 20:20:04 -0400

    Update api.py

--- api.py
+++ api.py
@@ -12,3 +12,11 @@ URL = 'https://opendata.arcgis.com/datasets/9fa34e198ad240358c7c36bc063d2058_40.
 data = requests.get(URL)
 
 print(data.status_code)
+
+#RID/Race/offesce type/offense/severity - is the severity of similar crimes the same by race?
+
+#cases by race - how many cases are there per race
+
+#type by age and gender
+
+#Race/offense type/sentence type/sentence imposted month - are sentences given similar for similar crimes by race?
